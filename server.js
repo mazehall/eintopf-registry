f2e552325196f3a29a1b6513838dec756fbd8772
Marcel Kilian <cdskilian@gmail.com>
2015-09-14 14:38:24 +0200

    added openshift support

--- server.js
+++ server.js
@@ -1,7 +1,9 @@
 var server = require('./app.js');
 
-var port;
-port = process.env.PORT || 3000
-server.listen(port, function() {
+var port, ip;
+port = process.env.OPENSHIFT_NODEJS_PORT || process.env.PORT || 3000;
+ip = process.env.OPENSHIFT_NODEJS_IP || '127.0.0.1';
+
+server.listen(port, ip, function() {
   console.log('server listen on port: ' + port);
 });
