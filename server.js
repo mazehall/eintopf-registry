ca6418cee81dbb8fb150e8b592243fbcaf9e477d
Marcel Kilian <cdskilian@gmail.com>
2015-10-14 14:31:55 +0200

    changed default listen ip to 0.0.0.0

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ var server = require('./app.js');
 
 var port, ip;
 port = process.env.OPENSHIFT_NODEJS_PORT || process.env.PORT || 3000;
-ip = process.env.OPENSHIFT_NODEJS_IP || '127.0.0.1';
+ip = process.env.OPENSHIFT_NODEJS_IP || '0.0.0.0';
 
 server.listen(port, ip, function() {
   console.log('server listen on port: ' + port);
